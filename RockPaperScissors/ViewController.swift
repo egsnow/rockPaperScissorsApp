0b016ab01d4a93587e236304cd877b83aa283448
Eric Snow <ericsnow@Medias-iMac.local>
2018-10-12 20:56:19 -0500

    completed RPS app

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -10,50 +10,54 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
-    
     @IBOutlet weak var playerScore: UILabel!
     @IBOutlet weak var computerScore: UILabel!
     @IBOutlet weak var playerImage: UIImageView!
     @IBOutlet weak var computerImage: UIImageView!
+    @IBOutlet weak var rulesImage: UIImageView!
+    
     
     let rock = 0
     let paper = 1
     let scissors = 2
     let rules = 3
-    
-    
+    var compScore = 0
+    var playScore = 0
     
     //0 = computer wins
     //1 = player wins
     //2 = tie
-    
-    
-    
+
     var playerChoice = true
     var computerChoice = false
 
     @IBAction func rockButton(_ sender: UIButton) {
         let compSelection = Int.random(in: 0...2)
-
         playGame(playSelection: 0, computerSelection: compSelection)
-
+        updateScore()
     }
     
     @IBAction func paperButton(_ sender: UIButton) {
         let compSelection = Int.random(in: 0...2)
-
         playGame(playSelection: 1, computerSelection: compSelection)
-
-
+        updateScore()
     }
     
     @IBAction func scissorButton(_ sender: UIButton) {
         let compSelection = Int.random(in: 0...2)
-
         playGame(playSelection: 2, computerSelection: compSelection)
-
+        updateScore()
     }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        playerImage.image = #imageLiteral(resourceName: "Empty")
+        computerImage.image = #imageLiteral(resourceName: "Empty")
+        rulesImage.image = #imageLiteral(resourceName: "Rules")
+        compScore = 0
+        playScore = 0
+        updateScore()
+    }
+    
     
     
     
@@ -62,12 +66,15 @@ class ViewController: UIViewController {
         case 0:
             print("rock")
             playerImage.image = #imageLiteral(resourceName: "rock")
+            rulesImage.image = #imageLiteral(resourceName: "Empty")
         case 1:
             print("paper")
             playerImage.image = #imageLiteral(resourceName: "paper")
+            rulesImage.image = #imageLiteral(resourceName: "Empty")
         case 2:
             print("scissors")
             playerImage.image = #imageLiteral(resourceName: "scissors")
+            rulesImage.image = #imageLiteral(resourceName: "Empty")
         default:
             break
         }
@@ -76,21 +83,26 @@ class ViewController: UIViewController {
         case 0:
             print("rock")
             computerImage.image = #imageLiteral(resourceName: "rock")
+            rulesImage.image = #imageLiteral(resourceName: "Empty")
         case 1:
             print("paper")
             computerImage.image = #imageLiteral(resourceName: "paper")
+            rulesImage.image = #imageLiteral(resourceName: "Empty")
         case 2:
             print("scissors")
             computerImage.image = #imageLiteral(resourceName: "scissors")
+            rulesImage.image = #imageLiteral(resourceName: "Empty")
         default:
             break
         }
         
         if (playSelection + 1) % 3 == computerSelection {
             print("computer wins")
+            compScore += 1
             return 0
         } else if (computerSelection + 1) % 3 == playSelection {
             print("player wins")
+            playScore += 1
             return 1
         } else {
             print("tie")
@@ -103,7 +115,10 @@ class ViewController: UIViewController {
     }
     
     
-
+    func updateScore() {
+        playerScore.text = "Player: \(playScore)"
+        computerScore.text = "Computer: \(compScore)"
+    }
         
         //update computer image based on random selection of 0, 1 or 2
         

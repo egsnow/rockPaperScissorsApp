ef9c9b68705e487f486dd3661618f10009d8eb0b
Eric Snow <ericsnow@Medias-iMac.local>
2018-11-08 14:51:27 -0600

    consolidated player selection buttons into one IBAction Function.

--- RockPaperScissors/ViewController.swift
+++ RockPaperScissors/ViewController.swift
@@ -31,23 +31,13 @@ class ViewController: UIViewController {
     var playerChoice = true
     var computerChoice = false
 
-    @IBAction func rockButton(_ sender: UIButton) {
+    @IBAction func playerButtonPressed(_ sender: UIButton) {
         let compSelection = Int.random(in: 0...2)
-        playGame(playSelection: 0, computerSelection: compSelection)
+        playGame(playSelection: sender.tag, computerSelection: compSelection)
         updateScore()
     }
+
     
-    @IBAction func paperButton(_ sender: UIButton) {
-        let compSelection = Int.random(in: 0...2)
-        playGame(playSelection: 1, computerSelection: compSelection)
-        updateScore()
-    }
-    
-    @IBAction func scissorButton(_ sender: UIButton) {
-        let compSelection = Int.random(in: 0...2)
-        playGame(playSelection: 2, computerSelection: compSelection)
-        updateScore()
-    }
     
     @IBAction func resetButton(_ sender: UIButton) {
         playerImage.image = #imageLiteral(resourceName: "Empty")
@@ -60,8 +50,7 @@ class ViewController: UIViewController {
     
     
     
-    
-    func playGame(playSelection: Int, computerSelection: Int) -> Int {
+    func playGame(playSelection: Int, computerSelection: Int) {
         switch playSelection {
         case 0:
             print("rock")
@@ -78,6 +67,8 @@ class ViewController: UIViewController {
         default:
             break
         }
+        
+        
         
         switch computerSelection {
         case 0:
@@ -96,23 +87,19 @@ class ViewController: UIViewController {
             break
         }
         
+        
+        
         if (playSelection + 1) % 3 == computerSelection {
             print("computer wins")
             compScore += 1
-            return 0
         } else if (computerSelection + 1) % 3 == playSelection {
             print("player wins")
             playScore += 1
-            return 1
         } else {
             print("tie")
-            return 2
         }
-        
-        
-        
-        
     }
+    
     
     
     func updateScore() {
